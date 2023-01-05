a012e36c5225a448cd67c10be517a42c7e6d9bbb
coldvl <volodymyr.holodnik@gmail.com>
2023-01-05 11:16:51 +0200

    added padding instead of Text(' ')

--- lib/main.dart
+++ lib/main.dart
@@ -15,45 +15,40 @@ void main() {
         '/Overall': (context) => const OverallStatus(),
         '/statistics': (context) => const Statistics(),
         '/HomeApp': (context) => const HomeApp(),
-
       },
     ),
   );
 }
 
 class Main extends StatelessWidget {
-  const Main({Key? key}) : super(key: key) ;
+  const Main({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 255, 168,1),
+      backgroundColor: const Color.fromARGB(255, 255, 168, 1),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Image(
-              image: AssetImage('assets/logo2.png'),),
-            const Text(''),
-            const Text(''),
-            const Text(''),
-            const Text(''),
-            const Text(''),
-            const Text(''),
-
-
-            const  Text('Workograph',style: TextStyle(fontSize: 30.0,  fontStyle: FontStyle.italic),),
-            const Text(''),
-            const Text(''),
-            const Text(''),
-            const Text(''),
-            const Text(''),
-            const Text(''),
-            const Text(''),
-            const Text(''),
-            const Text(''),
+            Padding(
+              padding: const EdgeInsets.all(30),
+              child: Image.asset(
+                'assets/logo2.png',
+                width: 250,
+                height: 250,
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(30),
+              child: Text("Workograph",
+                  style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic,
+                      color: Colors.black)),
+            ),
             TextButton.icon(
               style: TextButton.styleFrom(
-
                 backgroundColor: Colors.black,
                 foregroundColor: Colors.white,
                 padding: const EdgeInsets.all(20.0),
@@ -68,23 +63,20 @@ class Main extends StatelessWidget {
             const Text(''),
             TextButton.icon(
               style: TextButton.styleFrom(
-
-                  backgroundColor: Colors.black,
-                  foregroundColor: Colors.white,
+                backgroundColor: Colors.black,
+                foregroundColor: Colors.white,
                 padding: const EdgeInsets.all(20.0),
                 minimumSize: const Size(250.0, 30.0),
               ),
               onPressed: () {
                 Navigator.pushNamed(context, '/Overall');
               },
-
               icon: const Icon(Icons.arrow_forward_ios_outlined),
               label: const Text('Overall status'),
             ),
             const Text(''),
             TextButton.icon(
               style: TextButton.styleFrom(
-
                 backgroundColor: Colors.black,
                 foregroundColor: Colors.white,
                 padding: const EdgeInsets.all(20.0),
