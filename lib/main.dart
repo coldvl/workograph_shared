a16c53fe49f56934c5526fcdd596b156a2d14936
coldvl <volodymyr.holodnik@gmail.com>
2023-01-05 10:29:37 +0200

    updated main to the final version

--- lib/main.dart
+++ lib/main.dart
@@ -1,91 +1,104 @@
 import 'package:flutter/material.dart';
+import 'page_1.dart';
+import 'statistics.dart';
+import 'overall.dart';
+import 'clock.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const Main(),
+        '/page_1': (context) => const Pageone(),
+        '/Overall': (context) => const OverallStatus(),
+        '/statistics': (context) => const Statistics(),
+        '/HomeApp': (context) => const HomeApp(),
+
+      },
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class Main extends StatelessWidget {
+  const Main({Key? key}) : super(key: key) ;
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+      backgroundColor: const Color.fromARGB(255, 255, 168,1),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            const Image(
+              image: AssetImage('assets/logo2.png'),),
+            const Text(''),
+            const Text(''),
+            const Text(''),
+            const Text(''),
+            const Text(''),
+            const Text(''),
+
+
+            const  Text('Workograph',style: TextStyle(fontSize: 30.0,  fontStyle: FontStyle.italic),),
+            const Text(''),
+            const Text(''),
+            const Text(''),
+            const Text(''),
+            const Text(''),
+            const Text(''),
+            const Text(''),
+            const Text(''),
+            const Text(''),
+            TextButton.icon(
+              style: TextButton.styleFrom(
+
+                backgroundColor: Colors.black,
+                foregroundColor: Colors.white,
+                padding: const EdgeInsets.all(20.0),
+                minimumSize: const Size(250.0, 30.0),
+              ),
+              onPressed: () {
+                Navigator.pushNamed(context, '/page_1');
+              },
+              icon: const Icon(Icons.arrow_forward_ios_outlined),
+              label: const Text('    Continue    '),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            const Text(''),
+            TextButton.icon(
+              style: TextButton.styleFrom(
+
+                  backgroundColor: Colors.black,
+                  foregroundColor: Colors.white,
+                padding: const EdgeInsets.all(20.0),
+                minimumSize: const Size(250.0, 30.0),
+              ),
+              onPressed: () {
+                Navigator.pushNamed(context, '/Overall');
+              },
+
+              icon: const Icon(Icons.arrow_forward_ios_outlined),
+              label: const Text('Overall status'),
+            ),
+            const Text(''),
+            TextButton.icon(
+              style: TextButton.styleFrom(
+
+                backgroundColor: Colors.black,
+                foregroundColor: Colors.white,
+                padding: const EdgeInsets.all(20.0),
+                minimumSize: const Size(250.0, 30.0),
+              ),
+              onPressed: () {
+                Navigator.pushNamed(context, '/statistics');
+              },
+              icon: const Icon(Icons.arrow_forward_ios_outlined),
+              label: const Text('    Statistics    '),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
